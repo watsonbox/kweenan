49bac617f91ecc7b285a7496df44c2ea69aa552d
Howard Wilson <howard@smartdate.com>
2011-06-15 16:19:21 +0200

    Fix case sensitivity Postgres bug when searching brands

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -2,9 +2,10 @@ class BrandsController < ApplicationController
   skip_before_filter :require_user_data
   
   def index
-    @brands = Brand.where("name like ?", "%#{params[:q]}%")
-      respond_to do |format|
-        format.json { render :json => @brands.map(&:attributes) }
-      end
+    @brands = Brand.where("UPPER(name) like ?", "%#{params[:q].upcase}%") if params[:q].present?
+    
+    respond_to do |format|
+      format.json { render :json => (@brands || []).map(&:attributes) }
+    end
   end
 end
