feb714b990bb35e0ae312d2f3ce36bb47b81f644
Howard Wilson <howard@smartdate.com>
2011-06-15 15:49:30 +0200

    Use free databases for seeding brands and business categories

--- app/models/business_category.rb
+++ app/models/business_category.rb
@@ -2,6 +2,8 @@ class BusinessCategory < ActiveRecord::Base
   validate :name, :unique => true, :present => true
   has_many :merchants
   
+  default_scope :order => 'name'
+  
   def to_s
     name
   end
