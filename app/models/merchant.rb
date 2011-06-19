f047c435b8a4fa2e97863900b502a8e5aa1baaf6
Howard Wilson <howard@smartdate.com>
2011-06-19 16:44:09 +0200

    Add TOS check box to sign-up form and re-work error translations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,11 @@
 class Merchant < ActiveRecord::Base
-  attr_accessible :name, :address, :city, :postcode, :phone, :email, :hours, :description, :business_category_id, :brand_tokens
-  validates_presence_of :name, :address, :city, :postcode, :business_category_id
+  attr_accessible :name, :address, :city, :postcode, :phone, :hours, :description, :business_category_id, :brand_tokens
+  validates_presence_of :business_category
+  validates :name, :presence => true, :length => { :maximum => 128 }
+  validates :address, :presence => true, :length => { :maximum => 255 }
+  validates :city, :presence => true, :length => { :maximum => 64 }
+  validates :postcode, :presence => true, :length => { :is => 5 }, :numericality => true
+  
   has_one :user
   belongs_to :business_category
   has_many :carryings, :dependent => :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  validates :terms, :acceptance => { :allow_nil => false, :message => 'user.terms.accepted' }, :on => :create
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :merchant_id, :type, :merchant
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :merchant_id, :type, :merchant, :terms
   
   belongs_to :merchant
   has_one :profile, :class_name => 'UserProfile'
