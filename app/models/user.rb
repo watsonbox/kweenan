3e2a750fba0d5be5f3e7f304f01017ff78cabd1b
Howard Wilson <howard@smartdate.com>
2011-06-21 08:37:32 +0200

    Remove terms and conditions confirmation from merchant sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
-  validates :terms, :acceptance => { :allow_nil => false, :message => 'user.terms.accepted' }, :on => :create
+
+  validates :terms, :acceptance => { :allow_nil => false, :message => 'user.terms.accepted' }, :on => :create, :if => lambda { |c| c.type == 'Customer' }
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :merchant_id, :type, :merchant, :terms
   
