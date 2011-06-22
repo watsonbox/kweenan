864f49b4d96009134ece38edaa41b0bd26aa07af
Howard Wilson <howard@smartdate.com>
2011-06-22 10:58:06 +0200

    Revert "Remove terms and conditions confirmation from merchant sign-up"
    This reverts commit 3e2a750fba0d5be5f3e7f304f01017ff78cabd1b.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
-
-  validates :terms, :acceptance => { :allow_nil => false, :message => 'user.terms.accepted' }, :on => :create, :if => lambda { |c| c.type == 'Customer' }
+  validates :terms, :acceptance => { :allow_nil => false, :message => 'user.terms.accepted' }, :on => :create
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :merchant_id, :type, :merchant, :terms
   
